817b2dc2dcd860a34f625f20328f188604f342b0
caiobacode <caioodsf@gmail.com>
2023-04-17 23:15:52 -0300

    feat: criando middlewares - Req 6, 7

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 import express from 'express';
 import { insertProduct, getProducts } from './controllers/productsController';
 import { insertUser, login } from './controllers/usersController';
+import { authProductName, authProductAmount } from './middlewares/productsMiddleware';
+import authUserFields from './middlewares/userMiddleware';
 import getOrders from './controllers/ordersController';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/products', insertProduct);
+app.post('/products', authProductName, authProductAmount, insertProduct);
 
 app.get('/products', getProducts);
 
-app.post('/users', insertUser);
+app.post('/users', authUserFields, insertUser);
 
 app.get('/orders', getOrders);
 
---
+++ src/middlewares/userMiddleware.ts
@@ -0,0 +1,53 @@
+import { Request, Response, NextFunction } from 'express';
+
+function authString(ramdomString: string, thisString: string, ch: number) {
+  if (!ramdomString) {
+    return { type: 400, message: `"${thisString}" is required` };
+  }
+  if (typeof ramdomString !== 'string') {
+    return { type: 422, message: `"${thisString}" must be a string` };
+  }
+  if (ramdomString.length < ch) {
+    return { type: 422, message: `"${thisString}" length must be at least ${ch} characters long` };
+  }
+  return { type: 200, message: 'success' };
+}
+
+function authLevel(level: number) {
+  if (level === undefined) {
+    return { type: 400, message: '"level" is required' };
+  }
+  if (typeof level !== 'number') {
+    return { type: 422, message: '"level" must be a number' };
+  }
+  if (level < 1) {
+    return { type: 422, message: '"level" must be greater than or equal to 1' };
+  }
+  return { type: 200, message: 'success' };
+}
+
+async function authUserFields(req: Request, res: Response, next: NextFunction) {
+  const { username, vocation, password, level } = req.body;
+  if (authString(username, 'username', 3).type !== 200) {
+    const { type, message } = authString(username, 'username', 3);
+    return res.status(type).json({ message });
+  }
+
+  if (authString(vocation, 'vocation', 8).type !== 200) {
+    const { type, message } = authString(vocation, 'vocation', 3);
+    return res.status(type).json({ message });
+  }
+
+  if (authString(password, 'password', 8).type !== 200) {
+    const { type, message } = authString(password, 'password', 8);
+    return res.status(type).json({ message });
+  }
+
+  if (authLevel(level).type !== 200) {
+    const { type, message } = authLevel(level);
+    return res.status(type).json({ message });
+  }
+  next();
+}
+
+export default authUserFields;
